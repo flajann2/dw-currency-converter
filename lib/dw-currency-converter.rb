a6dfe78a4a05c89bf61460bd5e3645c9a35d6bdd
Fred Mitchell <fred.mitchell@gmx.de>
2016-11-09 20:00:35 +0100

    completed

--- lib/dw-currency-converter.rb
+++ lib/dw-currency-converter.rb
@@ -16,18 +16,54 @@ module DwCurrencyConv
     end
 
     def convert_to(currency)
-      if currency == @@base_currency # converting to base currency
-        Money.new(self.amount * 1.0 / @@rate_list[self.currency], currency)
-      elsif self.currency == @@base_currency # converting from the base currency
-        Money.new(self.amount * @@rate_list[self.currency] / 1.0, currency)
-      else # converting between currencies -- first conver to base, then from base to target
-        convert_to(@@base_currency).convert_to(currency)
+      begin
+        if currency == @@base_currency # converting to base currency
+          Money.new(self.amount * 1.0 / @@rate_list[self.currency], currency)
+        elsif self.currency == @@base_currency # converting from the base currency
+          Money.new(self.amount * @@rate_list[currency] / 1.0, currency)
+        else # converting between currencies -- first conver to base, then from base to target
+          convert_to(@@base_currency).convert_to(currency)
+        end
+      rescue => e
+        raise "Problem with converting #{self.currency} to #{currency}: #{e}"
       end
     end
     
     # Add these two objects and yield a third.
     def +(other_money)
-      Money.new(self.amount + other_money.amount, self.currency)
-    end    
+      Money.new(amount +
+                other_money.convert_to(currency).amount,
+                self.currency)
+    end
+    
+    def -(other_money)
+      Money.new(self.amount -
+                other_money.convert_to(self.currency).amount,
+                self.currency)
+    end
+    
+    def *(x)
+      Money.new(self.amount * x, self.currency)
+    end
+    
+    def /(x)
+      Money.new(self.amount / x, self.currency)
+    end
+    
+    def >(other_money)
+      self.amount > other_money.convert_to(self.currency).amount
+    end
+    
+    def <(other_money)
+      self.amount < other_money.convert_to(self.currency).amount
+    end
+
+    def ==(other_money)
+      amount.round(2) == other_money.convert_to(currency).amount.round(2)
+    end
+
+    def inspect
+      "%.2f #{currency}" % [amount.round(2)]
+    end
   end  
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -5,27 +5,59 @@ include DwCurrencyConv
 describe Money do
   before(:all) do
     Money.conversion_rates 'EUR', {
-                             'USD'     => 1.100855,
+                             'USD'     => 1.11,
                              'Bitcoin' => 0.0047,
                              'PHP'     => 53.768438,
                              'PLN'     => 4.34457705
                            }
+    @a = Money.new 20, 'EUR'
+    @b = Money.new 40, 'USD'
+    @fifty_eur = Money.new 50, 'EUR'
+    @twenty_dollars = Money.new 20, 'USD'
   end
   
-  it "created a money object" do
-    expect(Money.new(20.00, 'EUR')).to_not be_nil
-  end
-
   it "can add two currencies" do
-    a = Money.new 20, 'EUR'
-    b = Money.new 40, 'USD'
-    expect(a + b)
+    a_plus_b = @a + @b
+    expect(a_plus_b.inspect).to eq("56.04 EUR")
   end
 
   it "can convert between non-base currencies" do
     from = Money.new 2000, 'PHP'
     to = from.convert_to 'USD'
-    expect((to.amount * 100).floor).to eq(3719)
+    expect(to.amount.round(2)).to eq(41.29)
   end
-  
+
+  it "get amount and currency" do
+    expect(@fifty_eur.amount).to eq(50)
+    expect(@fifty_eur.currency).to eq('EUR')
+    expect(@fifty_eur.inspect).to eq('50.00 EUR')
+  end
+
+  it "convert to a different currency" do
+    expect(@fifty_eur.convert_to('USD').inspect).to eq('55.50 USD')
+  end
+
+  it "does arithemetics" do
+    expect((@fifty_eur + @twenty_dollars).inspect).to eq('68.02 EUR')
+    expect((@fifty_eur - @twenty_dollars).inspect).to eq('31.98 EUR')
+    expect((@fifty_eur / 2).inspect).to               eq('25.00 EUR')
+    expect((@twenty_dollars * 3).inspect).to          eq('60.00 USD')    
+  end
+
+  it "does comparasions" do
+    expect(@twenty_dollars == Money.new(20, 'USD')).to be_truthy
+    expect(@twenty_dollars == Money.new(30, 'USD')).to be_falsey
+
+    fifty_eur_in_usd = @fifty_eur.convert_to('USD')
+    
+    expect(fifty_eur_in_usd == @fifty_eur).to          be_truthy
+    expect(@twenty_dollars > Money.new(5, 'USD')).to   be_truthy
+    expect(@twenty_dollars < @fifty_eur).to             be_truthy
+  end
+
+  it "throws an exception if currency is not recognized" do
+    expect {
+      @twenty_dollars.convert_to('XXX')
+    }.to raise_error /Problem with converting EUR to XXX/
+  end
 end
