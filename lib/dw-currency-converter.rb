da78a6ca3e2ebe9117ef23daed6d01c02c5cfb59
Fred Mitchell <fred.mitchell@gmx.de>
2016-11-09 18:13:22 +0100

    conversion logic works

--- lib/dw-currency-converter.rb
+++ lib/dw-currency-converter.rb
@@ -7,6 +7,7 @@ module DwCurrencyConv
     def self.conversion_rates base, rate_list
       @@base_currency = base.to_s
       @@rate_list = rate_list.to_h
+      @@rate_list[base] = 1.0 # to simplify conversion logic
     end
     
     def initialize amount, currency
@@ -14,12 +15,19 @@ module DwCurrencyConv
       @currency = currency.to_s
     end
 
+    def convert_to(currency)
+      if currency == @@base_currency # converting to base currency
+        Money.new(self.amount * 1.0 / @@rate_list[self.currency], currency)
+      elsif self.currency == @@base_currency # converting from the base currency
+        Money.new(self.amount * @@rate_list[self.currency] / 1.0, currency)
+      else # converting between currencies -- first conver to base, then from base to target
+        convert_to(@@base_currency).convert_to(currency)
+      end
+    end
+    
     # Add these two objects and yield a third.
     def +(other_money)
       Money.new(self.amount + other_money.amount, self.currency)
-    end
-    
-  end
-  
+    end    
+  end  
 end
-
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -5,7 +5,6 @@ include DwCurrencyConv
 describe Money do
   before(:all) do
     Money.conversion_rates 'EUR', {
-                             'EUR'     => 1.0,
                              'USD'     => 1.100855,
                              'Bitcoin' => 0.0047,
                              'PHP'     => 53.768438,
@@ -22,4 +21,11 @@ describe Money do
     b = Money.new 40, 'USD'
     expect(a + b)
   end
+
+  it "can convert between non-base currencies" do
+    from = Money.new 2000, 'PHP'
+    to = from.convert_to 'USD'
+    expect((to.amount * 100).floor).to eq(3719)
+  end
+  
 end
